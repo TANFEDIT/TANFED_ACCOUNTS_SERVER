6b0fc3dc3f24755bcdf97330be2bbe1046068674
TANFEDIT <tanfeditteam@gmail.com>
2025-09-06 12:11:24 +0530

    feign clients url fix

--- src/main/java/com/tanfed/accounts/service/UserService.java
+++ src/main/java/com/tanfed/accounts/service/UserService.java
@@ -12,7 +12,7 @@ import com.tanfed.accounts.model.Office;
 
 
 
-@FeignClient(name = "USER-SERVICE", url = "${{USER_SERVICE_URL}")
+@FeignClient(name = "USER-SERVICE", url = "${USER_SERVICE_URL}")
 public interface UserService {
 	
 	@GetMapping("/auth/getofficelist")
